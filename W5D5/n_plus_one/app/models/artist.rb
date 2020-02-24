4fc8f7c273990df5d20ebf71a620ccff51a457c5
TERRENCE LAI <terrencelai@TERRENCEs-MacBook-Air.local>
2020-02-24 14:13:54 -0500

    W5D5 Homework

---
+++ W5D5/n_plus_one/app/models/artist.rb
@@ -0,0 +1,24 @@
+class Artist < ApplicationRecord
+  has_many :albums,
+    class_name: 'Album',
+    foreign_key: :artist_id,
+    primary_key: :id
+
+  def n_plus_one_tracks
+    albums = self.albums
+    tracks_count = {}
+    albums.each do |album|
+      tracks_count[album.title] = album.tracks.length
+    end
+
+    tracks_count
+  end
+
+  def better_tracks_query
+    self.albums
+    .includes(:tracks)
+    .select('albums.title, count(tracks.id)')
+    .group('albums.title')
+    
+  end
+end
